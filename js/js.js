4756db9dcedcc486151d98a3157f0151b1651c37
thuanphat2009 <anhxanh2019@gmail.com>
2022-01-27 14:55:26 +0700

    scroll

--- js/js.js
+++ js/js.js
@@ -2,10 +2,26 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const tabBtn = $$('.tab-pane')
-console.log(tabBtn)
+const tabList = $('.tab-list')
+const heightToTabList = $('.tab-list').offsetTop
+
 tabBtn.forEach(function(value,index){
     value.onclick = function(){
         $('.tab-pane.active').classList.remove('active')
         this.classList.add('active')
     }
 })
+
+window.onscroll = function(){
+    if (document.body.scrollTop >= 5300 && document.body.scrollTop < 5930 || document.documentElement.scrollTop >= 5300 && document.body.scrollTop < 5930) {
+        tabList.classList.add('scroll-tab')
+      }
+    else{
+        tabList.classList.remove('scroll-tab')
+
+      }
+}
+
+
+
+
